31c4fc79effe1b434ff04db81d092eddb03dfc77
Fadliabdill <fadliabdill287@gmail.com>
2025-03-02 18:11:24 +0700

    menambahkan migrasi

---
+++ database/migrations/2025_03_02_105031_create_barang_masuks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang_masuks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('produk_id');
+            $table->date('tgl_masuk');
+            $table->integer('harga_modal');
+            $table->integer('qty');
+            $table->string('satuan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang_masuks');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect('login');
+});
 
 // Route::view('dashboard', 'dashboard')
 //     ->middleware(['auth', 'verified'])
@@ -14,6 +16,6 @@ Route::view('profile', 'profile')
     ->name('profile');
 
 
-    Route::get('dashboard', Dashboard::class)->name('dashboard');
+Route::get('dashboard', Dashboard::class)->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
